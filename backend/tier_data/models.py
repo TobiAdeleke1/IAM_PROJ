fb4e0af78777020f7acc972033f087444eb0b672
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-03-05 22:41:54 +0000

    add planning application model

--- backend/tier_data/models.py
+++ backend/tier_data/models.py
@@ -34,3 +34,26 @@ class PricePaid(models.Model):
 
     def __str__(self):
         return f"[Price Paid at {self.postcode}, {self.town_or_city}]"
+
+
+class PlanningApplications(models.Model):
+    region = models.CharField(max_length=128)
+    local_planning_authority_name = models.CharField(max_length=128)
+    local_planning_authority_code = models.CharField(max_length=128)
+    quarter = models.CharField(max_length=20)
+    fiscal_year = models.CharField(max_length=20)
+    total_decisions_grand_total_all = models.IntegerField()
+    total_granted_grand_total_all = models.IntegerField()
+    total_refused_grand_total_all = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["local_planning_authority_name"]
+        indexes = [
+            models.Index(fields=["local_planning_authority_name"])
+        ]
+
+    def __str__(self):
+        return (f"[Planning Applications at"
+                f"{self.local_planning_authority_name}, {self.quarter}]")
