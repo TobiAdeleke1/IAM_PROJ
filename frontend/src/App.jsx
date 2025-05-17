05edddea93f3d047c27e459bd57970793f20ca0d
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-05-17 22:32:49 +0100

    add basic two panel frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import SideMenu from './components/SideMenu';
+
+
+export default function App() {
+
+  return (
+    <Box sx={{display : 'flex'}}>
+
+     <SideMenu />
+    </Box>
+
+  )
+}
+
