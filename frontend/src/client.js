366bd2753f0248adbd36e8523446cebb1898bd38
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-07-18 23:52:59 +0100

    add planning application data request

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -31,3 +31,21 @@ export const getPricePaidData = async ( getAccessTokenSilently) => {
 
     return response.json();
 };
+
+export const getPlanningApplicationData = async( getAccessTokenSilently) =>{
+    const token = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/tier_api/planningapplication`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+        },
+    });
+
+    if(!response.ok){
+        console.log("planningapplication:", response.message);
+        throw new Error(`${response.message} with error ${response.status}`);
+    }
+    return response.json();
+
+};
